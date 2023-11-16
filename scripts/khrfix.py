6dee8ffc2a292d004c54f56487151a4dfedd2b3c
wcde <65939220+wcde@users.noreply.github.com>
2023-11-16 10:46:59 +0300

    allow choose out_blocks behavior

--- scripts/khrfix.py
+++ scripts/khrfix.py
@@ -36,15 +36,17 @@ class KohyaHiresFix(scripts.Script):
             with gr.Row():
                 s2 = gr.Slider(minimum=0, maximum=16, step=1, label="Stop step", value=5)
                 d2 = gr.Slider(minimum=2, maximum=10, step=1, label="Depth", value=4)
-            scale = gr.Slider(minimum=1.0, maximum=4.0, step=0.1, label="Scale", value=2.0)
+            with gr.Row():
+                scale = gr.Slider(minimum=1.0, maximum=4.0, step=0.1, label="Scale", value=2.0)
+                early_out = gr.Checkbox(label="Upsample to native resolution early", value=True)
         
-        ui = [enable, d1, d2, s1, s2, scale]
+        ui = [enable, d1, d2, s1, s2, scale, early_out]
         for elem in ui:
             setattr(elem, "do_not_save_to_config", True)
         return ui
     
 
-    def process(self, p, enable, d1, d2, s1, s2, scale):
+    def process(self, p, enable, d1, d2, s1, s2, scale, early_out):
         if not enable or self.disable:
             script_callbacks.remove_current_script_callbacks()
             return
@@ -53,17 +55,19 @@ class KohyaHiresFix(scripts.Script):
         self.p1 = (s1, d1)
         self.p2 = (s1, d2)
         self.scale = scale
+        self.early_out = early_out
         
         def denoiser_callback(params: script_callbacks.CFGDenoiserParams):
             for s, d in [self.p1, self.p2]:
+                out_d = d if self.early_out else -d
                 if params.sampling_step < s:
                     if not isinstance(model.input_blocks[d], Scaler):
                         model.input_blocks[d] = Scaler(0.5, model.input_blocks[d])
-                        model.output_blocks[-d] = Scaler(self.scale, model.output_blocks[-d])
+                        model.output_blocks[out_d] = Scaler(self.scale, model.output_blocks[out_d])
                     return
                 elif isinstance(model.input_blocks[d], Scaler) and (self.p1[1] != self.p2[1] or s == self.p2[0]):
                     model.input_blocks[d] = model.input_blocks[d].block
-                    model.output_blocks[-d] = model.output_blocks[-d].block
+                    model.output_blocks[out_d] = model.output_blocks[out_d].block
                 
         script_callbacks.on_cfg_denoiser(denoiser_callback)
         
@@ -73,4 +77,4 @@ class KohyaHiresFix(scripts.Script):
                 p.sd_model.model.diffusion_model.input_blocks[i] = b.block
         for i, b in enumerate(p.sd_model.model.diffusion_model.output_blocks):
             if isinstance(b, Scaler):
-                p.sd_model.model.diffusion_model.output_blocks[-i] = b.block
+                p.sd_model.model.diffusion_model.output_blocks[i if args[-1] else -i] = b.block
