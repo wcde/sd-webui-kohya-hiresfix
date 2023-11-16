db9193882c78f662bed405e3b5c2a2631374b57b
wcde <65939220+wcde@users.noreply.github.com>
2023-11-16 13:43:35 +0300

    downsample

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 #### Stop step - at which sampling step disable fix, increase at higher resolution
 #### Depth - on which layer fix will be applied
-#### Upsampling scale - can slightly improve quality at cost of VRAM
+#### Downsampling scale - decrease at higher resolution, helps preserve composition at very high resolutions
+#### Upsampling scale - increasing can slightly improve quality at cost of VRAM
 
 ## Source:
 https://gist.github.com/kohya-ss/3f774da220df102548093a7abc8538ed
--- scripts/khrfix.py
+++ scripts/khrfix.py
@@ -34,22 +34,23 @@ class KohyaHiresFix(scripts.Script):
                 enable = gr.Checkbox(label='Enable extension', value=False)
                 only_one_pass = gr.Checkbox(label='Disable for additional passes', value=False)
             with gr.Row():
-                s1 = gr.Slider(minimum=0, maximum=12, step=1, label="Stop step", value=2)
+                s1 = gr.Slider(minimum=0, maximum=12, step=1, label="Stop step", value=3)
                 d1 = gr.Slider(minimum=2, maximum=10, step=1, label="Depth", value=3)
             with gr.Row():
                 s2 = gr.Slider(minimum=0, maximum=16, step=1, label="Stop step", value=5)
                 d2 = gr.Slider(minimum=2, maximum=10, step=1, label="Depth", value=4)
             with gr.Row():
-                scale = gr.Slider(minimum=1.0, maximum=4.0, step=0.1, label="Upsampling scale", value=2.0)
+                downscale = gr.Slider(minimum=0.1, maximum=1.0, step=0.05, label="Downsampling scale", value=0.5)
+                upscale = gr.Slider(minimum=1.0, maximum=4.0, step=0.1, label="Upsampling scale", value=2.0)
                 early_out = gr.Checkbox(label="Early upsampling (affects performance/quality)", value=True)
         
-        ui = [enable, only_one_pass, d1, d2, s1, s2, scale, early_out]
+        ui = [enable, only_one_pass, d1, d2, s1, s2, downscale, upscale, early_out]
         for elem in ui:
             setattr(elem, "do_not_save_to_config", True)
         return ui
     
 
-    def process(self, p, enable, only_one_pass, d1, d2, s1, s2, scale, early_out):
+    def process(self, p, enable, only_one_pass, d1, d2, s1, s2, downscale, upscale, early_out):
         if not enable or self.disable:
             script_callbacks.remove_current_script_callbacks()
             return
@@ -57,7 +58,8 @@ class KohyaHiresFix(scripts.Script):
         if s1 > s2: s2 = s1
         self.p1 = (s1, d1)
         self.p2 = (s1, d2)
-        self.scale = scale
+        self.downscale = downscale
+        self.upscale = upscale
         self.early_out = early_out
         self.only_one_pass = only_one_pass
         self.step_limit = 0
@@ -68,8 +70,8 @@ class KohyaHiresFix(scripts.Script):
                 out_d = d if self.early_out else -d
                 if params.sampling_step < s:
                     if not isinstance(model.input_blocks[d], Scaler):
-                        model.input_blocks[d] = Scaler(0.5, model.input_blocks[d])
-                        model.output_blocks[out_d] = Scaler(self.scale, model.output_blocks[out_d])
+                        model.input_blocks[d] = Scaler(self.downscale, model.input_blocks[d])
+                        model.output_blocks[out_d] = Scaler(self.upscale, model.output_blocks[out_d])
                     return
                 elif isinstance(model.input_blocks[d], Scaler) and (self.p1[1] != self.p2[1] or s == self.p2[0]):
                     model.input_blocks[d] = model.input_blocks[d].block
